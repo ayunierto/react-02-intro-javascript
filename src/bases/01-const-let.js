cb59c10562460a0bfa195f724fd26e6819b91db9
Alcides Turruellas <ayunierto@gmail.com>
2024-09-12 07:24:33 -0500

    var and const

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,14 @@
+const name = "Alcides";
+const lastName = "Turruellas";
+
+let value = 3;
+
+console.log(name, lastName, value);
+
+if (true) {
+  const name = "Peter";
+
+  console.log(name);
+}
+
+console.log(name);
